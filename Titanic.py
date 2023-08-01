b8ef1030aefdf5142420e3fdafc9cffefab7d695
Bruce0921 <114181725+Bruce0921@users.noreply.github.com>
2023-07-31 19:30:39 -0700

    kaggle projects of Titanic
    first try with random forest model with 4 factors

---
+++ Titanic.py
@@ -0,0 +1,6 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import os
+train_data = pd.read_csv("train.csv")
+print(train_data.head())
